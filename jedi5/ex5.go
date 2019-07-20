45d95082b39e6d07df79c32e28fdbdc05b69f8c2
Mikita Kryvitski <solairerove@gmail.com>
2019-07-20 20:31:50 +0300

    feature: using ok comma to ex5

--- jedi5/ex5.go
+++ jedi5/ex5.go
@@ -7,4 +7,20 @@ import (
 // Ex5 tbd
 func Ex5() {
 	fmt.Println("\nEx5()")
+
+	c := make(chan int)
+
+	go func() {
+		c <- 42
+	}()
+
+	v, ok := <-c
+
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+
+	fmt.Println(v, ok)
 }
