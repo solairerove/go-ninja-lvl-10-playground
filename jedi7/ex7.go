b4e1f584d3c714c8702adcb7ef8c14d6afc7357e
Mikita Kryvitski <solairerove@gmail.com>
2019-07-20 20:40:55 +0300

    feature: wtf

--- jedi7/ex7.go
+++ jedi7/ex7.go
@@ -7,4 +7,18 @@ import (
 // Ex7 tbd
 func Ex7() {
 	fmt.Println("\nEx7()")
+
+	c := make(chan int)
+
+	for j := 0; j < 10; j++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+			}
+		}()
+	}
+
+	for k := 0; k < 100; k++ {
+		fmt.Println(<-c)
+	}
 }
