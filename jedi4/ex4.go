45173ee85018aa396dc8b89604e538873de08df3
Mikita Kryvitski <solairerove@gmail.com>
2019-07-20 20:26:26 +0300

    feature: use select statement to complete ex4

--- jedi4/ex4.go
+++ jedi4/ex4.go
@@ -7,4 +7,33 @@ import (
 // Ex4 tbd
 func Ex4() {
 	fmt.Println("\nEx4()")
+
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+}
+
+func receive(c, q <-chan int) {
+	for {
+		select {
+		case v := <-c:
+			fmt.Println(v)
+		case <-q:
+			return
+		}
+	}
+}
+
+func gen(q chan<- int) <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		q <- 1
+	}()
+
+	return c
 }
