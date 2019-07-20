6d0522dacaa23ec2a063b642376289473670c592
Mikita Kryvitski <solairerove@gmail.com>
2019-07-20 20:16:28 +0300

    feature: complete ex3

--- jedi3/ex3.go
+++ jedi3/ex3.go
@@ -7,4 +7,26 @@ import (
 // Ex3 tbd
 func Ex3() {
 	fmt.Println("\nEx3()")
+
+	c := gen()
+	receive(c)
+}
+
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
+func gen() <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	return c
 }
